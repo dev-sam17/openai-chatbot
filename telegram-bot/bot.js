d699b7d95c841e53063fdc958c2ff7ca27f00a71
Developer Sam <dev_sam17@protonmail.com>
2023-05-15 17:20:37 +0000

    added openai api

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -1,17 +1,39 @@
-require('dotenv').config()
-
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const { sendResponse } = require('./chat');
 
 const token = process.env.TG_BOT_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText('/start', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Hello! This is your Telegram bot speaking.');
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
 });
 
-bot.onText('/sayhello', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Hello, human!');
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id;
+  try {
+    if(msg.text === '/start' || msg.text === '/sayhello') {
+        bot.sendMessage(chatId, 'Welcome to the ChatGPT telegram bot!');
+      } else {
+        const resp = await sendResponse(msg.text);
+      // send a message to the chat acknowledging receipt of their message
+      bot.sendMessage(chatId, resp);
+      }
+  } catch (error) {
+    bot.sendMessage(chatId, "Something unpected happened. Please try after sometime.");
+  }
+  
+  
 });
---
+++ telegram-bot/chat.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+require('dotenv').config()
+
+const API_URL = "https://api.openai.com/v1/chat/completions"
+
+async function sendResponse(input) {
+    let data = {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user" }],
+        temperature: 0.7
+    }
+
+    console.log('Prompt: ',input)
+    data.messages[0].content = input
+    
+    const config = {
+        headers: {
+            'Content-Type': "application/json",
+            'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        }
+    }
+
+    const response = await axios.post(API_URL, data, config)
+    const choices = response.data.choices[0]
+    const text = choices.message.content
+    console.log('Response: ',text)
+    return text
+}
+
+module.exports = {
+    sendResponse,
+}
